7fbc1e3ba92cbb0badf70e8a101108096c5c82e3
Bill Van Vooren <bvanvooren@hobiecat.com>
2019-08-11 18:30:15 -0700

    Change

--- saleor/hobie/views.py
+++ saleor/hobie/views.py
@@ -4,7 +4,7 @@ from .utils import export_orders_ready_to_fulfill_to_csv
 def export_orders_to_csv(request):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment;filename="hobie_cat_orders.csv"'
-    response.content = generate_csv_for_orders()      
+    response.content = export_orders_ready_to_fulfill_to_csv()      
 
     if len(response.content) == 0:
         raise Http404
