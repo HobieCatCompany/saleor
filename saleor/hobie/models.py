3b5f1a5f0c9e649c41bc8d771c45386d741aab11
Bill Van Vooren <bvanvooren@hobiecat.com>
2019-08-11 18:04:21 -0700

    Change

--- saleor/hobie/models.py
+++ saleor/hobie/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.timezone import now
 
 class ExportedOrder(models.Model):
-    id = models.AutoField()
+    id = models.AutoField(primary_key=True)
     order_id = models.IntegerField()
     created = models.DateTimeField(default=now, editable=False)
 
