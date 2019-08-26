ab9d276ca268e68657c9a2dc47044c853a24162a
Bill Van Vooren <bvanvooren@hobiecat.com>
2019-08-26 15:00:49 -0700

    Change

--- saleor/checkout/forms.py
+++ saleor/checkout/forms.py
@@ -322,8 +322,10 @@ class CheckoutShippingMethodForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         discounts = kwargs.pop("discounts")
         super().__init__(*args, **kwargs)
-        shipping_address = self.instance.shipping_address
-        country_code = shipping_address.country.code
+        #shipping_address = self.instance.shipping_address #HOBIE commented out
+        #country_code = shipping_address.country.code #HOBIE commendted out
+        country_code = 'US' #HOBIE
+        shipping_address = None #HOBIE 
         qs = ShippingMethod.objects.applicable_shipping_methods(
             price=calculate_checkout_subtotal(self.instance, discounts).gross,
             weight=self.instance.get_total_weight(),
