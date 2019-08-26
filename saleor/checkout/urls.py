b345130fb9f94dc71639830664f16dcce2efc46c
Bill Van Vooren <bvanvooren@hobiecat.com>
2019-08-26 12:41:52 -0700

    Change

--- saleor/checkout/urls.py
+++ saleor/checkout/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 from .views.discount import remove_voucher_view
 
+from ..hobie.views import 
+
 checkout_urlpatterns = [
     url(r"^$", views.checkout_index, name="index"),
     url(r"^start$", views.checkout_start, name="start"),
@@ -21,4 +23,7 @@ checkout_urlpatterns = [
     url(r"^dropdown/$", views.checkout_dropdown, name="dropdown"),
     url(r"^remove_voucher/", remove_voucher_view, name="remove-voucher"),
     url(r"^login/", views.checkout_login, name="login"),
+
+    #HOBIE below
+    url(r"^shipping/", views.checkout_order_summary, name="hobie-shipping"),
 ]
--- saleor/hobie/views.py
+++ saleor/hobie/views.py
@@ -1,6 +1,12 @@
 from django.http import Http404, HttpResponse
 from .utils import export_orders_ready_to_fulfill_to_csv
 
+from ..checkout.utils import (
+    get_checkout_context,
+    update_shipping_address_in_anonymous_checkout,
+    update_shipping_address_in_checkout,
+)
+
 def export_orders_to_csv(request):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment;filename="hobie_cat_orders.csv"'
@@ -10,3 +16,38 @@ def export_orders_to_csv(request):
         raise Http404
 
     return response
+
+#HOBIE from saleor/checkout/views.py
+@get_or_empty_db_checkout(Checkout.objects.for_display())
+@validate_voucher
+@validate_checkout
+@validate_is_shipping_required
+@add_voucher_form
+def shipping(request, checkout):
+    """Display the shipping step for a user who is not logged in."""
+    user_form, address_form, updated = update_shipping_address_in_anonymous_checkout(
+        checkout, request.POST or None, request.country
+    )
+
+    if updated:
+        return redirect("checkout:shipping-method")
+
+    ctx = get_checkout_context(checkout, request.discounts)
+    ctx.update({"address_form": address_form, "user_form": user_form})
+
+    discounts = request.discounts
+    is_valid_shipping_method(checkout, discounts)
+
+    form = CheckoutShippingMethodForm(
+        request.POST or None,
+        discounts=discounts,
+        instance=checkout,
+        initial={"shipping_method": checkout.shipping_method},
+    )
+    if form.is_valid():
+        form.save()
+        return redirect("checkout:summary")
+
+    ctx.update({"shipping_method_form": form})
+
+    return TemplateResponse(request, "hobie/shipping.html", ctx)
