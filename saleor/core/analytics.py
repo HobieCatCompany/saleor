ebfdd47438cfa343676fc30164a9c5ad792d84b6
Bill Van Vooren <bvanvooren@hobiecat.com>
2019-09-18 09:44:07 -0700

    Change

--- saleor/core/analytics.py
+++ saleor/core/analytics.py
@@ -68,7 +68,7 @@ def get_view_payloads(path, language, headers):
 
 
 def report_view(client_id, path, language, headers):
-    payloads = get_view_payloads(path, language, headers)
+    payloads = list(get_view_payloads(path, language, headers)) #HOBIE from https://github.com/mirumee/saleor/pull/3615/files
     extra_headers = {}
     user_agent = headers.get("HTTP_USER_AGENT", None)
     if user_agent:
